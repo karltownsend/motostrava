4f380173f30ed2351a2e5d4fa8f05e8aba83efd5
Karl Townsend <karltownsend@gmail.com>
2016-12-14 19:48:30 -0800

    update

--- app/hello.py
+++ app/hello.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+import numpy as np
+filename = 'laptimer.csv'
+lines_to_skip = 1    # first line in the header is the title, not for processing
+
+print 'ok, does this work?' # but only prints in console
+
+data = np.recfromcsv(filename, delimiter=',', skip_header=lines_to_skip)
 
 app = Flask(__name__)
 
@@ -9,6 +16,10 @@ def home():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+    
+@app.route('/python/')
+def python():
+    return render_template('python.html')
     
 if __name__ == '__main__':
     app.run(debug=True)
