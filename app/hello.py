652adb59bc9555e2c240a521cc546cd9e26ad0e2
Karl Townsend <karltownsend@gmail.com>
2016-12-16 16:55:01 -0800

    more changes

--- app/hello.py
+++ app/hello.py
@@ -1,11 +1,8 @@
-from flask import Flask, render_template
-import numpy as np
-filename = 'laptimer.csv'
-lines_to_skip = 1    # first line in the header is the title, not for processing
+from flask import Flask, render_template, flash, redirect
 
-print 'ok, does this work?' # but only prints in console
-
-data = np.recfromcsv(filename, delimiter=',', skip_header=lines_to_skip)
+# from Microblog
+# from app import app
+# from .forms import LoginForm
 
 app = Flask(__name__)
 
@@ -20,6 +17,40 @@ def about():
 @app.route('/python/')
 def python():
     return render_template('python.html')
+    
+@app.route('/python/microblog/')
+def microblog():
+    return render_template('microblog.html')
+    
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
     
 if __name__ == '__main__':
     app.run(debug=True)
